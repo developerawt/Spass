4beb5e3b6261e30a58aeb49733db89cd647da556
Your Name <ashish.samarth122@gmail.com>
2018-02-08 14:11:10 +0530

    Add Database Structure

--- app/src/main/java/offery/wizzo/in/offery/interfacefile/CountryPickerListener.java
+++
@@ -1,5 +0,0 @@
-package offery.wizzo.in.offery.interfacefile;
-
-public interface CountryPickerListener {
-	public void onSelectCountry(String name, String code, String dialCode);
-}
--- app/src/main/java/offery/wizzo/in/offery/interfacefile/TaskCompleteListener.java
+++
@@ -1,6 +0,0 @@
-package offery.wizzo.in.offery.interfacefile;
-
-public interface TaskCompleteListener {
-    public void onTaskComplete(int requestType, Object object);
-    public void onTaskError(int requestType, Object object);
-}
